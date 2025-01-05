fb8729c9957c0b5da9d38f41017aacfaaf786515
TLara1 <156106292+TLara1@users.noreply.github.com>
2025-01-05 21:18:38 +0000

    Deploying to gh-pages from @ TLara1/TLara1.github.io@70af7b6e552bedb6df9cd2b9b01424c7335ae9e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-notes",
           title: "Notes",
-          description: "A collection of notes I have compiled on interesting topics in physics and mathematics.",
+          description: "A collection of notes I have compiled on interesting topics in physics and mathematics. These are created mostly for my personal reference, they are more concise than course notes one may find on the same topic. I do not guarantee these are without mistakes, if you find an error, please let me know.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
