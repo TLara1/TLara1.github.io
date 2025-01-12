5cd45d2395a102db32e843484fc73dbcd924fecb
TLara1 <156106292+TLara1@users.noreply.github.com>
2025-01-12 20:33:44 +0000

    Deploying to gh-pages from @ TLara1/TLara1.github.io@8d5289a311281bde1a423c43563c136033f09649 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,9 +410,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-on-hertzian-contacts-of-two-disks",
+          title: 'On Hertzian Contacts of Two Disks',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
