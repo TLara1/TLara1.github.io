842075929b3a1d2ac7bf9b6d41c4e28f469eb3ff
TLara1 <156106292+TLara1@users.noreply.github.com>
2025-01-28 01:03:53 +0000

    Deploying to gh-pages from @ TLara1/TLara1.github.io@d73cc66ef4569d357a1ce332bb4e7ef80d99a4ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
