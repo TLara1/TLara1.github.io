0609c5fe34e8796edc77a3e887b1ceb26daa1a4d
TLara1 <156106292+TLara1@users.noreply.github.com>
2025-07-06 20:39:32 +0000

    Deploying to gh-pages from @ TLara1/TLara1.github.io@d99ffa5bb0fa6c4fde0076b2a79ae3fa2efa8716 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -432,9 +432,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-observing-walking-fluid-droplet-dynamics",
+          title: 'Observing Walking Fluid Droplet Dynamics',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
